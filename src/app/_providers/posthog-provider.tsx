ce47b282b54407303794f0593ff9c9e2ea59fdb5
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2025-06-14 21:19:36 +0530

    Analytics working

--- src/app/_providers/posthog-provider.tsx
+++ src/app/_providers/posthog-provider.tsx
@@ -4,13 +4,12 @@
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
-import { env } from "process";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/relay-g1u1",
-      ui_host: "https://us.posthog.com",
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      defaults: "2025-05-24",
     });
   }, []);
 
